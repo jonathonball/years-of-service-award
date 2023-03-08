c98c6bd1d17852d4ecd66952f1cc036fab9681c5
Jon Ball <jonathon.ball@gmail.com>
2023-03-07 22:24:49 -0500

    Filesystem module improvements

--- filesystem.py
+++ filesystem.py
@@ -1,17 +1,32 @@
 import os
 from memoize import memoize
+import sys
 
 extensions = ('.png', '.jpg', '.jpeg')
+env_prefix = 'YOSA'
+env_suffix = 'DIR'
+
+def validate_dir(path):
+  return path and os.path.isdir(path)
+
+def get_dir_path(dir_name):
+  envvar_name = '_'.join([env_prefix, dir_name.upper(), env_suffix])
+  dir_path = os.getenv(envvar_name)
+  if validate_dir(dir_path):
+    return dir_path
+  script_path = os.path.dirname(os.path.abspath(__file__))
+  dir_path = os.path.join(script_path, dir_name)
+  if validate_dir(dir_path):
+    return dir_path
+  print(f"Unabled to load {dir_name}: {dir_path}")
 
 @memoize
 def get_app_dirs():
-  work_dir = os.environ.get('YOSA_WORK_DIR')
-  if not work_dir or not os.path.isdir(work_dir):
-    work_dir = os.path.dirname(os.path.abspath(__file__))
-  input_dir = os.path.join(work_dir, 'input')
+  input_dir = get_dir_path('input')
+  output_dir = get_dir_path('output')
   return {
-    'work': work_dir,
-    'input': input_dir
+    'input': input_dir,
+    'output': output_dir
   }
 
 def get_image_full_path(image_file):
