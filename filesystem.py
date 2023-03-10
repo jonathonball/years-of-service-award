146921f8ab4dc9f012ac029cf1787f56d6c18453
Jon Ball <jonathon.ball@gmail.com>
2023-03-10 09:12:21 -0500

    Add tmp dir to filesystem module

--- filesystem.py
+++ filesystem.py
@@ -22,13 +22,16 @@ def get_dir_path(dir_name):
 
 @memoize
 def get_app_dirs():
-  input_dir = get_dir_path('input')
+  input_dir  = get_dir_path('input')
   output_dir = get_dir_path('output')
+  tmp_dir    = get_dir_path('tmp')
   return {
-    'input': input_dir,
-    'output': output_dir
+    'input':  input_dir,
+    'output': output_dir,
+    'tmp':    tmp_dir,
   }
 
+# change this method to indicate it's for input only or refactor
 def get_image_full_path(image_file):
   dirs = get_app_dirs()
   return os.path.join(dirs['input'], image_file)
