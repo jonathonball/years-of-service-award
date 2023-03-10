306a7529838ebcf57d2c78a77f6a6d2fb424d148
Jon Ball <jonathon.ball@gmail.com>
2023-03-10 09:13:16 -0500

    Add infinite loop to pickup_images.py

--- pickup_images.py
+++ pickup_images.py
@@ -1,17 +1,22 @@
 import filesystem
 import hash
 import cache
+import time
 
 dirs       = filesystem.get_app_dirs()
 cache_conn = cache.init_cache()
 
-image_files = filesystem.get_image_files()
-for image_file in image_files:
-  image_path = filesystem.get_image_full_path(image_file)
-  image_hash = hash.calculate_file_md5(image_path)
-  cache_data = cache.get_file_data(cache_conn, image_hash)
-  if not cache_data:
-    cache_data = cache.create_file_data(image_file, image_path)
-    cache.set_file_data(cache_conn, image_hash, cache_data)
-    print(f'Picked up {str(image_hash)} ({str(image_file)})')
-    cache.add_to_queue(cache_conn, 'rotate', image_hash)
+while True:
+  image_files = filesystem.get_image_files()
+  for image_file in image_files:
+    image_path = filesystem.get_image_full_path(image_file)
+    image_hash = hash.calculate_file_md5(image_path)
+    cache_data = cache.get_file_data(cache_conn, image_hash)
+    if not cache_data:
+      cache_data = cache.create_file_data(image_file, image_path)
+      cache.set_file_data(cache_conn, image_hash, cache_data)
+      print(f'Picked up {str(image_hash)} ({str(image_file)})')
+      queue_name = cache.get_first_queue_name()
+      cache.add_to_queue(cache_conn, queue_name, image_hash)
+      print(f'Image {image_hash} added to "{queue_name}" queue')
+  time.sleep(1)
