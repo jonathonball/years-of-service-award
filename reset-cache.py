8648cf919116e34b380a8aa0c19fa0bf35dd5792
Jon Ball <jonathon.ball@gmail.com>
2023-03-11 20:19:52 -0500

    Add -y --yes aliases and fix tabs

--- reset-cache.py
+++ reset-cache.py
@@ -1,17 +1,18 @@
+#!/usr/bin/env python3
 import redis
 import argparse
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--force', '-f', action='store_true', help='skip confirmation prompt')
+parser.add_argument('--force', '--yes', '-y', '-f', action='store_true', help='skip confirmation prompt')
 args = parser.parse_args()
 
 if not args.force:
   confirmation = input("Are you sure you want to delete all data in the Redis cache? (Yes/No)")
 
 if args.force or confirmation.lower() in ['yes', 'y']:
-    r.flushall()
-    print("All data in the Redis cache has been deleted.")
+  r.flushall()
+  print("All data in the Redis cache has been deleted.")
 else:
-    print("Data deletion aborted.")
+  print("Data deletion aborted.")
