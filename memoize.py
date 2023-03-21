08aed95996d5bf547e9ee4947a6126f1fefd23ac
Jon Ball <jonathon.ball@gmail.com>
2023-03-20 20:53:47 -0400

    Fix linter bad-indentation errors

--- memoize.py
+++ memoize.py
@@ -3,14 +3,14 @@ Functions wrappers to cache results between calls
 """
 
 def memoize(func):
-  """
-  Place function results into a dict and return it if called again
-  """
-  cache = {}
+    """
+    Place function results into a dict and return it if called again
+    """
+    cache = {}
 
-  def wrapper():
-    if () not in cache:
-      cache[()] = func()
-    return cache[()]
+    def wrapper():
+        if () not in cache:
+            cache[()] = func()
+        return cache[()]
 
-  return wrapper
+    return wrapper
