e5c58d0772567e0e4c401881d91bc3d679178d6a
Jon Ball <jonathon.ball@gmail.com>
2023-03-20 20:54:55 -0400

    Fix invalid-name lint error

--- checksum.py
+++ checksum.py
@@ -8,7 +8,7 @@ def calculate_file_md5(filename):
     Calculate md5sum of a file, reading it 4K bytes at a time
     """
     hash_md5 = hashlib.md5()
-    with open(filename, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
+    with open(filename, "rb") as file_handle:
+        for chunk in iter(lambda: file_handle.read(4096), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
